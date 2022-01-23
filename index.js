662d8bf7940d8396f867c06a14f2c06cf85e6aeb
Nathan Fallet <contact@nathanfallet.me>
2022-01-23 17:05:19 +0100

    Limiting the matrix to be daily

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
 * Fonctions utiles pour la génération
 */
 
+const customRandom = function(s) {
+    var mask = 0xffffffff;
+    var m_w  = (123456789 + s) & mask;
+    var m_z  = (987654321 - s) & mask;
+
+    return function() {
+      m_z = (36969 * (m_z & 65535) + (m_z >>> 16)) & mask;
+      m_w = (18000 * (m_w & 65535) + (m_w >>> 16)) & mask;
+
+      var result = ((m_z << 16) + (m_w & 65535)) >>> 0;
+      result /= 4294967296;
+      return result;
+    }
+}(Math.ceil(Date.now() / (24*3600*1000)))
+
 function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min
+    return Math.floor(customRandom() * (max - min)) + min
 }
 
 function gcdExtended(a, b) {
